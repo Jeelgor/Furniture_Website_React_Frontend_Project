27feba2533a83ea530b8ee99d84c222b856b05c3
Gor Jeel <jeelgor10@gmail.com>
2024-01-22 15:42:27 +0530

    React

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// App.jsx
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="Login-Box">
+        <h3>Sign In</h3>
+        <div className="AllTextField">
+          <div className="textfield">
+            <label>UserEmail</label>
+            <input type="text" placeholder="Email" />
+          </div>
+          <div className="textfield">
+            <label>UserPassword</label>
+            <input type="text" placeholder="Password" />
+          </div>
+          <button>Sign In</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
