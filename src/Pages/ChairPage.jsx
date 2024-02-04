f20ae8e3abcb8e3ec8d6e643b4d458577ca1519a
Gor Jeel <jeelgor10@gmail.com>
2024-02-04 10:55:19 +0530

    Update_4

--- src/Pages/ChairPage.jsx
+++ src/Pages/ChairPage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "../Styles/OurProduct.css";
 import Card from "../component/Card";
-import Study1 from "../assets/StudyTable/Study1.png";
-import Study2 from "../assets/StudyTable/Study2.png";
-import Study3 from "../assets/StudyTable/Study3.png";
-import Study4 from "../assets/StudyTable/Study4.png";
-import Study5 from "../assets/StudyTable/Study5.png";
-import Study6 from "../assets/StudyTable/Study6.png";
+import chair1 from "../assets/Chair/chair1.png";
+import chair2 from "../assets/Chair/chair2.png";
+import chair3 from "../assets/Chair/chair3.png";
+import chair4 from "../assets/Chair/chair4.png";
+import chair5 from "../assets/Chair/chair5.png";
+import chair6 from "../assets/Chair/chair6.png";
+import chair7 from "../assets/Chair/chair7.png";
+import chair8 from "../assets/Chair/chair8.png";
+
 import Footer from "../component/Footer";
 const ChairPage = () => {
   return (
@@ -21,17 +24,17 @@ const ChairPage = () => {
       </div>
       {/* Card Section */}
       <div className="FirstThree">
-        <Card ImageSrc={Study1} title="Chair1" price="₹3000" />
-        <Card ImageSrc={Study2} title="Chair2" price="₹4000" />
-        <Card ImageSrc={Study3} title="Chair3" price="₹5000" />
-        <Card ImageSrc={Study4} title="Chair4" price="₹6000" />
+        <Card ImageSrc={chair1} title="Chair" price="₹3000" />
+        <Card ImageSrc={chair2} title="Chair" price="₹4000" />
+        <Card ImageSrc={chair3} title="Chair" price="₹5000" />
+        <Card ImageSrc={chair4} title="Chair" price="₹6000" />
 
       </div>
       <div className="SecondThree">
-        <Card ImageSrc={Study4} title="Chair4" price="₹6000" />
-        <Card ImageSrc={Study5} title="Chair4" price="₹6000" />
-        <Card ImageSrc={Study6} title="Chair4" price="₹6000" />
-        <Card ImageSrc={Study6} title="Chair4" price="₹6000" />
+        <Card ImageSrc={chair5} title="Chair" price="₹7000" />
+        <Card ImageSrc={chair6} title="Chair" price="₹8000" />
+        <Card ImageSrc={chair7} title="Chair" price="₹9000" />
+        <Card ImageSrc={chair8} title="Chair" price="₹10000" />
       </div>
       <Footer/>
     </>
