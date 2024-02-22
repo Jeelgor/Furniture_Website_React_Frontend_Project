8914762a522b6c7fb5df3f44aabf0a9293731346
Gor Jeel <jeelgor10@gmail.com>
2024-02-22 08:35:39 +0530

    Update

---
+++ FURNITURE/Data/FurnitureDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FURNITURE.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FURNITURE.Data
+{
+    public class FurnitureDbContext : DbContext
+    {
+        public FurnitureDbContext(DbContextOptions<FurnitureDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<FurnitureDataModel> FurnitureData { get; set; }
+    }
+}
+
